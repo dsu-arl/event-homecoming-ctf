3d6d449b5c9ee999d9546f0cc16f6c45dcb59b6c
chase <cjopsahl99@gmail.com>
2025-09-18 13:48:57 -0500

    remove extra space

--- forensics/findthegeneralii/ans.py
+++ forensics/findthegeneralii/ans.py
@@ -1,3 +1,2 @@
 OPTS = ["a pet shark", "mrbeast's youtube channel", "a first edition holographic charizard"]
 ANS = ["a pet shark", "pet shark", "mrbeast's youtube channel", "a first edition holographic charizard", "first edition holographic charizard"]
-
