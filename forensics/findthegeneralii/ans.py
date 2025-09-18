3f60884693ec9856aca56ce5970778b1e8f7ecd9
chase <cjopsahl99@gmail.com>
2025-09-18 13:25:04 -0500

    hopeful fix

--- forensics/findthegeneralii/ans.py
+++ forensics/findthegeneralii/ans.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-import random
 OPTS = ["a pet shark", "mrbeast's youtube channel", "a first edition holographic charizard"]
 ANS = ["a pet shark", "pet shark", "mrbeast's youtube channel", "a first edition holographic charizard", "first edition holographic charizard"]
 
-def main():
-    with open('./secret_plan.txt', 'a') as fObj:
-        fObj.write(f"Step 3. Use dollerz to buy {random.choice(OPTS)}\n")
-        fObj.write(f"Step 4. Do laundry\n")
-
-if __name__ == "__main__":
-    main()
