b1ef90af49d1c8f3eb076235e8e48dce01849bc6
chase <cjopsahl99@gmail.com>
2025-09-18 14:03:48 -0500

    plz

--- forensics/findthegeneralii/ans.py
+++
@@ -1,2 +0,0 @@
-OPTS = ["a pet shark", "mrbeast's youtube channel", "a first edition holographic charizard"]
-ANS = ["a pet shark", "pet shark", "mrbeast's youtube channel", "a first edition holographic charizard", "first edition holographic charizard"]
